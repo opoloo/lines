d028bbc30eb3ab2fb4cb156e7f70316065ff2f2a
Sandra Scherer <sandra@opoloo.de>
2013-09-09 15:55:06 +0200

    Initial Commit

---
+++ index.php
@@ -0,0 +1,119 @@
+<!doctype html>
+<html class="no-js">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="author" content="opoloo GbR" />
+<meta name="description" content="" />
+<meta name="keywords" content="" />
+<meta http-equiv="Content-Type"	content="text/html; charset=UTF-8" />
+<link href='http://fonts.googleapis.com/css?family=Gentium+Basic:400,400italic,700,700italic' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Ubuntu+Condensed|Ubuntu:400,300,500,700|Ubuntu+Mono' rel='stylesheet' type='text/css'>
+<meta name="viewport" content="width=device-width, minimum-scale=1, maximum-scale=1">
+
+<title>Lines | Simple Publishing on Rails</title>
+
+<!-- Embedding Favicon -->
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+<link rel="icon" href="favicon.png" type="image/png" />
+
+<!-- General Stylesheet -->
+<link href="stylesheets/stylesheet.css" rel="stylesheet" type="text/css" />
+
+<!-- Responsive -->
+<link href="stylesheets/media-queries.css" rel="stylesheet" type="text/css" />
+
+</head>
+
+
+<body>
+<a href="https://github.com/you"><img style="position: fixed; top: 0; right: 0; border: 0;z-index:9999;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png" alt="Fork me on GitHub"></a>
+<div id="meta">
+	<div class="wrapper">
+		<div id="logo">
+			<a href="/"><img src="images/logo.png" alt="Logo LINES" /></a>
+		</div>
+		<nav>
+			<ul>
+				<li><a href="">View</a></li>
+				<li><a href="">Editor</a></li>
+				<li><a href="">Features</a></li>
+			</ul>
+		</nav>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<header>
+	<div class="wrapper">
+		<div id="head">
+			<div id="teaser">
+				<h1>Simple Publishing on Rails</h1>
+			</div>
+			<div class="intro">
+				You've been waiting for this*
+				<p>*You are a Rails developer. You care about your writing and a beautiful, mobile-optimized presentation. You appreciate a solid editing system without unnecessary buzz.
+				Lines is a beautiful blog template for developers, built with Ruby on Rails.</p>
+			</div>
+		</div>
+	</div>
+</header>
+
+<content>
+	<div class="wrapper">
+		<div class="two-cols">
+			<h2>Features</h2>
+			<p>LINES is a blog framework for developers with a strong focus on a simple, consistent publishing process and clear, responsive presentation. It's customizable and puts your content first.</p>
+		</div>
+		<div class="two-cols">
+			<h2>Documentation</h2>
+			<p>We stuck closely to the code conventions, so you'll have no trouble modifying it to your needs. The complete documentation is on GitHub. We highly recommend a look at the readme file.</p>
+		</div>
+		<div class="clearfix"></div>
+		<hr/>
+		<div class="one-third">
+			<h2>Optimized for reading</h2>
+			<p>The best writing in the world amounts to nothing if you can't read it how you want it, where you want it. Lines is optimized for a clean reading experience, its responsive design scaling perfectly to big and small screens.</p>
+		</div>
+		<div class="two-thirds">
+		
+		</div>
+		<hr/>
+		<div id="features">
+			<div class="two-cols">
+				<h2>Simply the right features</h2>
+				<p>LINES is a blog framework for developers with a strong focus on a simple, consistent publishing process, on clear responsive presentation. It's customizable and puts your content first.</p>
+			</div>
+			<div class="two-cols">
+				<h2>Extendable</h2>
+				<p>Nearly everything about this blog template can be customized to your personal needs and preferences. It is based on the latest Ruby 3.2, which is all you need besides a database that supports it (for example MySQL).  We'll constantly maintain it and keep it up-to-date on GitHub.</p>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+		
+		<div class="three-cols">
+		
+		</div>
+		<div class="three-cols">
+		
+		</div>
+		<div class="three-cols">
+		
+		</div>
+		<hr/>
+		<div id="whatsnot">
+			<h3>What it's not</h3>
+			<p>LINES is a very young publishing platform, built up from the ground. It needs people, work, and time to mature. It won't replace your Wordpresses, Bloggers or Squarespaces anytime soon. Instead, LINES offers a solid starting point for Rails- and web-developers that want to cut the crap and focus on their content, with the freedom of full OSS access.</p>
+		</div>
+	</div>
+</content>
+<div id="footer">
+  <div id="copyright">
+	<span class="about">LINES is a project by opoloo. Find us on Google Plus & Twitter or write us an email to info@opoloo.de
+</span>
+	<span>All rights reserved &copy; 2013 <a href="http://www.opoloo.com">Opoloo GbR</a> | <a href="http://www.opoloo.de/impressum">Imprint</a></span>
+	<span class="signet"><a href="http://www.opoloo.com/"></a></span>
+  </div>
+</div>
+
+
+</body>
+</html>
