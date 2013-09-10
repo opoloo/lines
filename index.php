b7877a43391dd1eb639c1c503e5b40500e962e00
Sandra Scherer <sandra@opoloo.de>
2013-09-10 17:09:15 +0200

    New Copy, add Parallax Scrolling, fine-tuning typo

--- index.php
+++ index.php
@@ -22,6 +22,23 @@
 <!-- Responsive -->
 <link href="stylesheets/media-queries.css" rel="stylesheet" type="text/css" />
 
+<!-- Scripts -->
+<script type="text/javascript" src="scripts/jquery-1.10.2.min.js"></script>
+<script type="text/javascript" src="scripts/jquery.parallax-1.1.3.js"></script>
+
+
+
+<script type="text/javascript">
+$(document).ready(function(){
+	//.parallax(xPosition, speedFactor, outerHeight) options:
+	//xPosition - Horizontal position of the element
+	//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
+	//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
+	$('header').parallax("50%", 0.4);
+
+})
+</script>
+
 </head>
 
 
@@ -40,17 +57,17 @@
 		<div class="clearfix"></div>
 	</div>
 </div>
-<header>
+<header id="header">
 	<div class="wrapper">
 		<div id="head">
 			<div id="teaser">
-				Simple Publishing on Rails
+				Simple Publishing on Rails 3.2
 			</div>
 			<div class="intro">
 				<h1>The open source blog platform you* <br/>have been waiting for.</h1>
-				<p>*You are a Ruby on Rails developer. 
-				You care about your writing and a beautiful, mobile-optimized presentation. 	
-				You appreciate a solid editing system without unnecessary buzz. 		
+				<p>*You are a Ruby on Rails developer. <br/>
+				You care about your writing and a beautiful, mobile-optimized presentation.<br/>	
+				You appreciate a solid editing system without unnecessary buzz. 		<br/>
 				You want full control.</p>
 			</div>
 		</div>
@@ -59,19 +76,53 @@
 
 <div id="content">
 	<div class="wrapper">
-		<div id="features">
+		<div id="desc">
 			<h2>Enjoy simple, responsive, customizable publishing</h2>
 			<div class="three-cols">
-				<p>Manage</p>	
+				<img src="images/screenshot_manage.jpg" alt="Manage" />
+				<h3>Manage</h3>
+				<p>A clear, consistent frontend that focuses on what you need.</p>	
 			</div>
 			<div class="three-cols">
-				<p>Create & Edit</p>
+				<img src="images/screenshot_create.jpg" alt="Create & Edit" />
+				<h3>Create & Edit</h3>
+				<p>A slim, beautifully designed editor to get your ideas out. </p>
 			</div>
 			<div class="three-cols">
-				<p>Publish</p>
+				<img src="images/screenshot_publish.jpg" alt="Publish" />
+				<h3>Publish</h3>
+				<p>Your text, code examples, images and links up front.</p>
 			</div>
+			<div class="clearfix"></div>
 		</div>
-		<div class="clearfix"></div>
+		<hr/>
+		<div id="features">
+			<div class="two-cols">
+				<h2>What it is</h2>
+				<p>LINES is a blog framework for developers with a strong focus on a simple, consistent publishing process and clear, responsive presentation. It's customizable and puts your content first, with the freedom of full OSS access.</p>
+			</div>
+			<div class="two-cols">
+				<h2>What it's not</h2>
+				<p>Lines is not Wordpress, Blogger, or Squarespace and doesn't aim to be. It's not a gem or plugin — it's an app. No plugins, no theme craziness, no options-galore, just full focus on publishing and reading. </p>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+		<hr/>
+		<div class="one-col">
+			<h2>Optimized for reading</h2>
+			<p>The best writing in the world amounts to nothing if you can't read it how you want it, where you want it. Lines is optimized for a clean reading experience that scales perfectly to big and small screens.</p>
+			<ul>
+				<li>| clear & comprehensive overview of your posts</li>
+				<li>| header graphics for visual identification</li>
+				<li>| headlines optimized for scanning</li>
+				<li>| focus on links, images & code</li>
+			</ul>
+			
+		</div>
+		<div id="devices">
+			<img src="images/devices.png" alt="Devices" />
+		</div>
+		<hr/>
 		<div id="installation">
 			<h2>Installation</h2>
 			<div class="two-cols">
@@ -82,34 +133,19 @@
 			</div>
 			<div class="clearfix"></div>
 		</div>
-		<div class="clearfix"></div>
 		<hr/>
-		<div class="one-third">
-			<h2>Optimized for reading</h2>
-			<p>The best writing in the world amounts to nothing if you can't read it how you want it, where you want it. Lines is optimized for a clean reading experience, its responsive design scaling perfectly to big and small screens.</p>
-		</div>
-		<div class="two-thirds">
-		
-		</div>
-		<hr/>
-		<div id="features">
+		<div id="docu">
 			<div class="two-cols">
-				<h2>Simply the right features</h2>
-				<p>LINES is a blog framework for developers with a strong focus on a simple, consistent publishing process, on clear responsive presentation. It's customizable and puts your content first.</p>
+				<h2>Documentation</h2>
+				<p>We stuck closely to the code conventions, so you'll have no trouble modifying it to your needs. The complete documentation is on GitHub. Have a look at the <a href="#">readme</a>.</p>
 			</div>
 			<div class="two-cols">
-				<h2>Extendable</h2>
-				<p>Nearly everything about this blog template can be customized to your personal needs and preferences. It is based on the latest Ruby 3.2, which is all you need besides a database that supports it (for example MySQL).  We'll constantly maintain it and keep it up-to-date on GitHub.</p>
+				<h2>Customizable</h2>
+				<p>LINES is based on Rails 3.2 (4.0 to come soon) and can be customized to your personal needs and preferences. We'll constantly maintain the blog platform and keep it up-to-date it on <a href="#">GitHub</a>.</p>
 			</div>
 			<div class="clearfix"></div>
 		</div>
-		
-
 		<hr/>
-		<div id="whatsnot">
-			<h3>What it's not</h3>
-			<p>LINES is a very young publishing platform, built up from the ground. It needs people, work, and time to mature. It won't replace your Wordpresses, Bloggers or Squarespaces anytime soon. Instead, LINES offers a solid starting point for Rails- and web-developers that want to cut the crap and focus on their content, with the freedom of full OSS access.</p>
-		</div>
 	</div>
 </div>
 <div id="footer">
