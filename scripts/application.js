ce2f8a1939901212eed8dd5e722e15b3503186ec
Jochen Greif <jochen@opoloo.de>
2013-09-12 17:53:37 +0200

    updated to newest version

--- scripts/application.js
+++ scripts/application.js
@@ -13,4 +13,23 @@ $(document).ready(function(){
     }, 500);
     return false;
   });
-})
+
+  // Lightbox
+  $('#desc').magnificPopup({
+    delegate: 'a',
+    type: 'image',
+    tLoading: 'Loading image #%curr%...',
+    mainClass: 'mfp-img-mobile',
+    gallery: {
+      enabled: true,
+      navigateByImgClick: true,
+      preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+    },
+    image: {
+      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+      titleSrc: function(item) {
+        return item.el.attr('title') + '<small>Lines by Opoloo</small>';
+      }
+    }
+  });
+});
