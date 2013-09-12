a2d85a2774aee5decf665b6a3bfaa34e2387888f
Sandra Scherer <sandra@opoloo.de>
2013-09-12 12:11:51 +0200

    New Footer, new background images, new header

--- index.php
+++ index.php
@@ -34,7 +34,8 @@ $(document).ready(function(){
 	//xPosition - Horizontal position of the element
 	//inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
 	//outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
-	$('header').parallax("50%", 0.4);
+	$('#header').parallax("50%", 0.2);
+	$('#footer').parallax("50%", 0.4);
 
 
 })
@@ -55,6 +56,7 @@ $(document).ready(function(){
 				<li><a href="#installation">Installation</a></li>
 				<li><a href="#docu">Documentation</a></li>
 				<li><a href="#contribute">Contribute</a></li>
+				<li><a href="#contribute">LINES in action</a></li>
 				<li><a href="https://github.com/opoloo/lines"><img style="position: fixed; top: 0; right: 0; border: 0;z-index:999;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub"></a></li>
 			</ul>
 		<div class="clearfix"></div>
@@ -66,39 +68,40 @@ $(document).ready(function(){
 			<div id="teaser">
 				Simple Publishing on Rails 3.2
 			</div>
+			<hr/>
 			<div class="intro">
-				<h1>The open source blog platform you* <br/>have been waiting for.</h1>
+				<h1>LINES is the open source blog platform you<span>*</span> have been waiting for.</h1>
+				<hr/>
 				<p>*You are a Ruby on Rails developer. <br/>
 				You care about your writing and a beautiful, mobile-optimized presentation.<br/>	
 				You appreciate a solid editing system without unnecessary buzz. 		<br/>
 				You want full control.</p>
 			</div>
 		</div>
-	</div>
-</header>
-
-<div id="content">
-	<div class="wrapper">
 		<div id="desc">
 			<h2>Enjoy simple, responsive, customizable publishing</h2>
 			<div class="three-cols">
-				<img src="images/screenshot_manage.jpg" alt="Manage" />
+				<img src="images/header_01.png" alt="Manage" />
 				<h3>1. Manage</h3>
 				<p>Manage your posts in clear, consistent frontend.</p>	
 			</div>
 			<div class="three-cols">
-				<img src="images/screenshot_create.jpg" alt="Create & Edit" />
+				<img src="images/header_02.png" alt="Create & Edit" />
 				<h3>2. Create & Edit</h3>
 				<p>Splash around in your ideas in a slim, beautifully designed editor.</p>
 			</div>
 			<div class="three-cols nomargin">
-				<img src="images/screenshot_publish.jpg" alt="Publish" />
+				<img src="images/header_03.png" alt="Publish" />
 				<h3>3. Publish</h3>
 				<p>See your text, code examples, images, and links shine.</p>
 			</div>
 			<div class="clearfix"></div>
 		</div>
-		<hr/>
+	</div>
+</header>
+
+<div id="content">
+	<div class="wrapper">
 		<div id="features" class="white_box">
 			<div class="two-cols">
 				<h2>What it is</h2>
@@ -172,8 +175,9 @@ $(document).ready(function(){
 			</div>
 			<div class="clearfix"></div>
 		</div>
-		<hr/>
-		<div id="contribute">
+	</div>
+<div id="footer">
+		<div id="contribute" class="wrapper">
 				<h2>Contribute</h2>
 				<p>LINES is an open source project for a reason. It needs people, work, and time to mature. You can help make this project even greater by giving us feedback, telling us about your concerns, and voicing questions. Contact our developers:</p><br/>
 			<div class="two-cols table">
@@ -188,14 +192,9 @@ $(document).ready(function(){
 
 			</div>
 			<div class="clearfix"></div>
-		</div>
-		<hr/>
-	</div>
-</div>
-<div id="footer">
+		</div>		
   <div id="copyright">
 	<span class="about">LINES is a project by opoloo. Find us on <a href="https://plus.google.com/u/0/+Opoloo/posts">Google Plus</a> & <a href="https://twitter.com/Opoloo">Twitter</a> or write us an email to <script type="text/javascript">eval(decodeURIComponent('%64%6f%63%75%6d%65%6e%74%2e%77%72%69%74%65%28%27%3c%61%20%68%72%65%66%3d%5c%22%6d%61%69%6c%74%6f%3a%69%6e%66%6f%40%6f%70%6f%6c%6f%6f%2e%64%65%5c%22%3e%69%6e%66%6f%40%6f%70%6f%6c%6f%6f%2e%64%65%3c%5c%2f%61%3e%27%29%3b'))</script>
-.
 </span>
 	<span>All rights reserved &copy; 2013 <a href="http://www.opoloo.com">Opoloo GbR</a> | <a href="http://www.opoloo.de/impressum">Imprint</a></span>
 	<span class="signet"><a href="http://www.opoloo.com/"></a></span>
